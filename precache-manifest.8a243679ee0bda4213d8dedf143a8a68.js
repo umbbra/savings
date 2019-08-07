aa6023c25e7c8944d0916ba99c9fd3e4d1453e57
Patrycja Paczkowska <umbra@spoko.pl>
2019-08-07 12:54:42 +0200

    Updates

---
+++ precache-manifest.8a243679ee0bda4213d8dedf143a8a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31534d3dd9641969473a62c178aedd66",
+    "url": "/savings/index.html"
+  },
+  {
+    "revision": "b9031201a94a9755e78e",
+    "url": "/savings/static/css/main.84014948.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/savings/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "b9031201a94a9755e78e",
+    "url": "/savings/static/js/main.7f064ec4.chunk.js"
+  },
+  {
+    "revision": "79bbba99ef82e4f28f17",
+    "url": "/savings/static/js/runtime~main.caab05ac.js"
+  },
+  {
+    "revision": "ecff1f6997479c3d75ad9e6adfc6a4f4",
+    "url": "/savings/static/media/floor-tile.ecff1f69.png"
+  },
+  {
+    "revision": "74bc2e4f04c6c4b81e062c2f2afe2127",
+    "url": "/savings/static/media/money-bgc.74bc2e4f.jpg"
+  }
+]);
